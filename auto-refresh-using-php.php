1f7f0a0058acab250ee865240260cada139a87ec
Shubham Balyan <sb1851994@gmail.com>
2019-05-04 23:59:23 +0530

    Add auto-refresh example using php

---
+++ auto-refresh-using-php.php
@@ -0,0 +1,16 @@
+<?php
+$page = $_SERVER['PHP_SELF'];
+$sec = "10";
+?>
+<html>
+    <head>
+    <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
+    </head>
+    <body>
+    <?php
+        echo "Watch the page reload itself in 10 second!";
+    ?>
+    </body>
+</html>
+
+
